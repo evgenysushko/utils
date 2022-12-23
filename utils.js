bd620c33060b20a120543474fbd51d6e0e99c4cf
Evgeny Sushko <evgeny.sushko@gmail.com>
2022-12-23 16:05:55 +0000

    more functions added
    - gerRandomArbitrary, getRandomInt, colorsEqual functions added
    - docstrings added

--- utils.js
+++ utils.js
@@ -1,3 +1,26 @@
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+/**
+* Returns a random integer between min (inclusive) and max (inclusive).
+* The value is no lower than min (or the next integer greater than min
+* if min isn't an integer) and no greater than max (or the next integer
+* lower than max if max isn't an integer).
+* Using Math.round() will give you a non-uniform distribution!
+*/
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/** 
+ * Creates a readable timestamp from a Unix timestamp
+ */
 function timestamp() {
   var date = new Date();
   var year = date.getFullYear();
@@ -15,4 +38,11 @@ function timestamp() {
   second = ("00" + second).substr(-2, 2);
 
   return [year, month, day, "_", hour, minute, second, "_", millis].join('');
+}
+
+/**
+ * Checks whether two colors are equal
+ */
+function colorsEqual(col1, col2) {
+  return col1.toString() == col2.toString()
 }
